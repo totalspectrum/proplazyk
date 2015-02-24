d0ae6d889bb2d6e5389b9d277eb136ee4eb1f5a8
Eric Smith <ersmith@totalspectrum.ca>
2015-02-24 11:51:37 -0400

    added hello world example

---
+++ lazier/eg/hello.scm
@@ -0,0 +1,15 @@
+(load "../lazier.scm")
+(load "../prelude.scm")
+(load "../prelude-numbers.scm")
+
+(lazy-def '(main input)
+ '(
+   ((cons 72) ((cons 101) ((cons 108) ((cons 108) ((cons 111) ((cons 44)
+   ((cons 32)
+   ((cons 119) ((cons 111) ((cons 114) ((cons 108) ((cons 100) ((cons 33)
+   ((cons 10)
+   ((cons 256) 256)))))))))))))))
+  )
+)
+
+(print-as-unlambda (laze 'main))
